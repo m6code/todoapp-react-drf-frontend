3159cc6b7752110635f7d276b5aed770881289a7
M6code <benjamynfrank101@gmail.com>
2020-05-09 19:47:22 +0100

    Updates

---
+++ precache-manifest.e1d46eee60c6d45d8002ea88740dd215.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b792a0b2de30371aef932531cd6b3591",
+    "url": "/todoapp-react-drf-frontend/index.html"
+  },
+  {
+    "revision": "a728f524402451a59b40",
+    "url": "/todoapp-react-drf-frontend/static/css/main.2efeafc2.chunk.css"
+  },
+  {
+    "revision": "e06749b951a6127843cb",
+    "url": "/todoapp-react-drf-frontend/static/js/2.0d551aa6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp-react-drf-frontend/static/js/2.0d551aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a728f524402451a59b40",
+    "url": "/todoapp-react-drf-frontend/static/js/main.96ba16da.chunk.js"
+  },
+  {
+    "revision": "833d8ef1052cff2a7ad3",
+    "url": "/todoapp-react-drf-frontend/static/js/runtime-main.30ae6484.js"
+  }
+]);
